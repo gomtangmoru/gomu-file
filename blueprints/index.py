62d52ea6c55f53276e84fc9acaf0cdec5a90ed46
JaeYoung Ryu <admin@gomtangmo.ru>
2025-06-10 01:13:31 +0900

    잘못된 코드형식과 계획되지 않은 기능이기에 이전 버전으로 되돌렸습니다

--- blueprints/index.py
+++ blueprints/index.py
@@ -10,18 +10,4 @@ bp = Blueprint('index', __name__,
 @bp.route('/')
 def index():
     return render_template('index.html')
-  
-# 로거 설정
-  logger = logging.getLogger(__name__)
-    #접속 로그 기록
-    logger.info("Main page accessed")
-  
-    # 로그인 인증 체크 (선택적 적용)
-    if 'user_id' not in session:
-        logger.warning("비로그인 사용자 접근")
-        return redirect(url_for('auth.login'))
-    # 템플릿에 현재 시간 전달
-    current_time = datetime.now()
 
-    # 템플릿 렌더링
-    return render_template('index.html', current_time=current_time)
